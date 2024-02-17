4836b74f5e17ef1a5197125c3c047e74f9d09bf2
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:23:52 +0500

    Add files via upload

---
+++ Q5.ts
@@ -0,0 +1,7 @@
+// Q5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+ let famous_person: string ="Hazrat Ali"
+
+ let message: string = " “When the world pushes you to your knees, you're in the perfect position to pray”"
+
+ console.log(`${famous_person} Once Said,${message}`)
